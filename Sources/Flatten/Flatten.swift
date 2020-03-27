16e38ab1ad71cd95135b7af94c7eaee11975f599
Yusuke Hosonuma <tobi462@gmail.com>
2020-03-27 15:35:00 +0900

    add: generate code and test

--- Sources/Flatten/Flatten.swift
+++ Sources/Flatten/Flatten.swift
@@ -1,3 +1,30 @@
-struct Flatten {
-    var text = "Hello, World!"
+func generate(argunmentCount: UInt) -> String {
+    let flatten0 = """
+    func flatten<V, R: Equatable>(_ f: @escaping (V) -> () -> R) -> (T) -> R {
+        { (v: V) -> R in f(t)() }
+    }
+    """
+
+    if argunmentCount == 0 {
+        return flatten0
+    } else {
+        let flattenN: [String] = (1 ... argunmentCount).map { number in
+            let range = 1 ... number
+
+            func list(_ f: (Int) -> String) -> String {
+                range.map { f(Int($0)) }.joined(separator: ", ")
+            }
+
+            let types = list { "T\($0)" }
+            let params = list { "t\($0): T\($0)" }
+            let args = list { "t\($0)" }
+
+            return """
+            func flatten<V, \(types), R>(_ f: @escaping (V) -> (\(types)) -> R) -> (V, \(types)) -> R {
+                { (v: V, \(params)) -> R in f(v)(\(args)) }
+            }
+            """
+        }
+        return ([flatten0] + flattenN).joined(separator: "\n\n")
+    }
 }
--- Tests/FlattenTests/FlattenTests.swift
+++ Tests/FlattenTests/FlattenTests.swift
@@ -1,15 +1,44 @@
 import XCTest
 @testable import Flatten
 
+/*
+ /// uncurry instance-method that has no arguments (e.g. `String#lowercase`)
+ func flatten<T, R: Equatable>(_ f: @escaping (T) -> () -> R) -> (T) -> R {
+     { (t: T) -> R in f(t)() }
+ }
+
+ /// uncurry instance-method that has arguments (e.g. `String#hasPrefix`)
+
+ func flatten<V, T1, R>(_ f: @escaping (V) -> (T1) -> R) -> ((V, T1) -> R) {
+     { (v: V, t1: T1) -> R in f(v)(t1) }
+ }
+
+ func flatten<V, T1, T2, R>(_ f: @escaping (V) -> (T1, T2) -> R) -> (V, T1, T2) -> R {
+     { (v: V, t1: T1, t2: T2) -> R in f(v)(t1, t2) }
+ }
+*/
+
 final class FlattenTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Flatten().text, "Hello, World!")
-    }
+        
+        assertEqualLines(generate(argunmentCount: 0), """
+        func flatten<V, R: Equatable>(_ f: @escaping (V) -> () -> R) -> (T) -> R {
+            { (v: V) -> R in f(t)() }
+        }
+        """)
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+        assertEqualLines(generate(argunmentCount: 2), """
+        func flatten<V, R: Equatable>(_ f: @escaping (V) -> () -> R) -> (T) -> R {
+            { (v: V) -> R in f(t)() }
+        }
+
+        func flatten<V, T1, R>(_ f: @escaping (V) -> (T1) -> R) -> (V, T1) -> R {
+            { (v: V, t1: T1) -> R in f(v)(t1) }
+        }
+
+        func flatten<V, T1, T2, R>(_ f: @escaping (V) -> (T1, T2) -> R) -> (V, T1, T2) -> R {
+            { (v: V, t1: T1, t2: T2) -> R in f(v)(t1, t2) }
+        }
+        """)
+    }
 }
